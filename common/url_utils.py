875fdc28db190d00a8890608a0b5847a80a1f3c9
kwsy <xigongda200608@163.com>
2020-02-23 11:21:29 +0800

    解析url

--- common/url_utils.py
+++ common/url_utils.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 def get_netloc(url):
     """
     解析url网址,获得网站的主页, 输入 http://www.coolpython.net/python_primary/data_type/bin_int_hex_oct.html
@@ -5,9 +7,11 @@ def get_netloc(url):
     :param url:
     :return:
     """
+    result = urlparse(url)
+    return result.scheme + "://" + result.netloc
 
 
 if __name__ == '__main__':
     url = 'http://www.coolpython.net/python_primary/data_type/bin_int_hex_oct.html'
     netloc = get_netloc(url)
-    print(url)
+    print(netloc)
