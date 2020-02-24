2d5c8ec21261d89051aa1d0fdcf333304e6a46fa
zmh19870712 <56657989@qq.com>
2020-02-24 17:01:04 +0800

    增加搜索关键词—_url解码—_去重

--- common/url_utils.py
+++ common/url_utils.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 def get_netloc(url):
     """
     解析url网址,获得网站的主页, 输入 http://www.coolpython.net/python_primary/data_type/bin_int_hex_oct.html
@@ -5,7 +6,9 @@ def get_netloc(url):
     :param url:
     :return:
     """
-
+    result = urlparse(url)
+    result_url = result.scheme + "://" + result.netloc
+    return result_url
 
 if __name__ == '__main__':
     url = 'http://www.coolpython.net/python_primary/data_type/bin_int_hex_oct.html'
--- crawler/baidu_crawler.py
+++ crawler/baidu_crawler.py
@@ -4,6 +4,8 @@ import requests
 from lxml import etree
 from urllib.parse import quote
 
+from conf.bd_keywords import keywords
+from common.url_utils import get_netloc
 
 def extract_links_test(filepath):
     """
@@ -39,6 +41,19 @@ def save_htmlfile(path_folder, keyword, num, html):
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(html)
 
+def save_urlfile(path_folder, keyword, url_list):
+    create_folder_htmlfile(path_folder)
+
+    keyword_folder = path_folder + "/" + keyword
+    create_folder_htmlfile(keyword_folder)
+
+    filename = "{0}/百度搜索分析url内容_{1}".format(keyword_folder, keyword)
+    with open(filename, 'w', encoding='utf-8') as f:
+        for str_url in url_list:
+            f.write(str_url)
+            f.write("\n")
+
+
 
 def generate_params(keyword, num):
     # 根据关键字生成字典params，用于sesion请求,pn根据页码数(n)变化，对应值为(n-1)*10
@@ -55,6 +70,26 @@ def generate_params(keyword, num):
 ''''''''''''''''''''''''''''''''''''''
 
 
+# 将百度链接转化为真实链接，并得到主页
+def url_baidu_to_realmain(url):
+    res = requests.get(url, allow_redirects=False)
+    real_url = res.headers['location']
+    #realmain_url = get_netloc(real_url)
+    #return realmain_url
+
+    return real_url
+def urlist_baidu_to_realmain(url_list):
+    realmain_url_list=[]
+    print(url_list)
+    for url in url_list:
+        print(url)
+        realmain_url = url_baidu_to_realmain(url)
+        #print("Real_url")
+        #print(realmain_url)
+        realmain_url_list.append(realmain_url)
+        #print("Real_url_list")
+        #print(realmain_url_list)
+    return realmain_url_list
 def extract_links(html):
     """
     从网页源码里解析出搜索结果的连接
@@ -64,7 +99,21 @@ def extract_links(html):
     tree = etree.HTML(html)
 
     url_list = tree.xpath("//div[@class='result c-container ']/h3/a/@href")
-    return url_list
+    print()
+    Real_url_list = urlist_baidu_to_realmain(url_list)
+    return Real_url_list
+
+def crawler_baidu_by_all_keyword(keywords):
+    url_all_keywords_list_temp=[]
+    for keyword in keywords:
+        url_keyword_list = crawler_baidu_by_keyword(keyword)
+        print("百度爬取关键字完毕_{0}".format(keyword))
+        save_urlfile(global_path_urlfile, keyword, url_keyword_list)
+
+        url_all_keywords_list_temp.extend(url_keyword_list)
+    #去重
+    url_all_keywords_list = list(set(url_all_keywords_list_temp))
+    save_urlfile(global_path_urlfile, "汇总", url_all_keywords_list)
 
 
 def crawler_baidu_by_keyword(keyword):
@@ -107,10 +156,13 @@ def crawler_baidu_by_keyword(keyword):
         # 对每个html页面分析，获取所有的url，添加到列表url_list
         url_list = extract_links(res.text)
 
-        url_total_list.extend(url_list)
+        url_keyword_list.extend(url_list)
+        print("url_total_list包含{}组链接".format(len(url_keyword_list)))
+        print(url_keyword_list)
+    return url_keyword_list
 
-        print("url_total_list包含{}组链接".format(len(url_total_list)))
-        print(url_total_list)
+
+
 
 
 if __name__ == '__main__':
@@ -118,16 +170,21 @@ if __name__ == '__main__':
     global_path_htmlfile = "./htmlfile"  # html存储文件夹
     global_save_htmlfileflag = 1
 
+    global_path_urlfile = "./urlfile" # url存储文件夹
+
     global_keyword = "c语言教程"
     global_totalnum_page = 3
     global_baidu_url = 'https://www.baidu.com/s'
 
+
     global_sleeptime = 1
 
-    url_total_list = []
-    crawler_baidu_by_keyword(global_keyword)
+    url_keyword_list = []
+    url_all_keywords_list = []
+    #crawler_baidu_by_keyword(global_keyword)
 
-
+    print(keywords)
+    crawler_baidu_by_all_keyword(keywords)
 
 
 
