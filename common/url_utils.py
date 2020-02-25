5165e8c2daf7769a914693d2909705cd563a1eca
wlxfen <wlxfen@163.com>
2020-02-26 00:24:42 +0800

    王立翔第二次提交url解析20200226

--- common/url_utils.py
+++ common/url_utils.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 def get_netloc(url):
     """
     解析url网址,获得网站的主页, 输入 http://www.coolpython.net/python_primary/data_type/bin_int_hex_oct.html
@@ -5,9 +6,10 @@ def get_netloc(url):
     :param url:
     :return:
     """
-    url_1=url.split("//")[1].split("/")[0]
-    url_2="http://"+url_1
+    url_1 = urlparse(url)
+    url_2 = url_1.scheme + "://"+url_1.hostname
     return url_2
+
 
 
 if __name__ == '__main__':
