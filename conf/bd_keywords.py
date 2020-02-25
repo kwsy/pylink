1099e40a36781e2ff43a990fe5d6259bb64b4e11
wlxfen <wlxfen@163.com>
2020-02-25 21:54:04 +0800

    王立翔conf提交20200225

--- conf/bd_keywords.py
+++ conf/bd_keywords.py
@@ -1,3 +1,9 @@
 """
 请在脚本里配置合适的搜索词
 """
+
+bd_keywords=["python教程","python论坛","python 个人博客","python社区",
+            "python学习","python项目","python开发","python基础",
+            "python简介","python视频","python问题","python程序",
+            "python专题","python网站","python自学","python交流",
+            "python大全","python培训","python学院","python答疑"]
