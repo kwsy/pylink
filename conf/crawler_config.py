85580afbc3ebf34c1d4f87db200dfbbe39d55815
kwsy <xigongda200608@163.com>
2020-02-25 21:04:28 +0800

    代码评审

--- conf/crawler_config.py
+++ conf/crawler_config.py
@@ -19,6 +19,7 @@ TOTALNUM_SEARCH_PAGE = 10                # 单个关键词搜索引擎爬取页�
 TIME_PAGE_SLEEP = 1                      # 爬取每页间隔时间
 TIME_GETREALURL_SLEEP = 0.001            # 获取真实url间隔时间
 
+# 这些都是程序里临时用到的,不是配置项,不要写在这里
 LIST_URL_ONEKEYWORD = []                 # 单个关键词搜索结果url列表
 LIST_URL_ALLKEYWORD = []                 # 所有关键词搜索结果url列表
 
