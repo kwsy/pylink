8439c58566f6f8414bb1ed060d48f64183b778e2
wlxfen <wlxfen@163.com>
2020-03-06 23:13:18 +0800

    第一次任务项目代码完整

--- crawler/baidu_crawler.py
+++ crawler/baidu_crawler.py
@@ -1,5 +1,10 @@
 import requests
 from lxml import etree
+from urllib.parse import quote
+
+
+
+
 def crawler_baidu_by_keyword(keyword):
     """
     根据关键词抓取百度搜索结果
@@ -8,19 +13,29 @@ def crawler_baidu_by_keyword(keyword):
     """
 
     headers ={
-              'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-              'Accept - Encoding': 'gzip, deflate, br',
-              'Accept - Language': 'zh-CN, zh;en-US;q=0.8,en;q=0.7',
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+              'Accept - Encoding': 'gzip, deflate, compress',
+              'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
               'Cache - Control': 'max-age = 0',
               'Connection': 'keep-alive',
+              'referer': quote('http://www.baidu.com/s?wd=python&pn=10'),
+              'Host': 'www.baidu.com',
               'User - Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
              }
 
-    url ='https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd='+keyword
-
-    res =requests.get(url,headers=headers)
+    url ='https://www.baidu.com/s'
+    params = {
+        'wd': keyword,
+        'pn':  0
+             }
+    session = requests.session()
+    res = session.get(url, params=params, headers=headers,allow_redirects=False)
+    res.encoding = 'utf-8'
+    print(res.status_code)
     print(res.text)
-     # return lst
+    link_lst =extract_links(res.text)
+
+    return link_lst
 
 def extract_links(html):
     """
@@ -28,17 +43,20 @@ def extract_links(html):
     :param html:
     :return:
     """
+    url_lst=[]
     tree = etree.HTML(html)
+    a_nodes=tree.xpath("//div[@class='result c-container ']/h3/a[@data-click]")
 
-    a_nodes_2=tree.xpath("//div[@id='content_left']//a[@data-click and @href]/@href")
-    print(a_nodes_2)
+    for a in a_nodes:
+        url_lst.append(a.attrib['href'])
+    return url_lst
 
-    return a_nodes_2
+
 
 if __name__ == '__main__':
-    # crawler_baidu_by_keyword('python 教程')
-    # lst = crawler_baidu_by_keyword('python 教程')
-    # print(lst)
+     # with open("../baidu.txt",'rb')as f:
+     #     print(extract_links(f.read()))
 
-    with open("../baidu.txt",'rb')as f:
-        extract_links(f.read())
+     lst = crawler_baidu_by_keyword('python 教程')
+     print(lst)
+
