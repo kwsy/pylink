886c50fc3b6b46d4020abfd4e5681d9c0da478fc
kwsy <xigongda200608@163.com>
2020-03-07 09:18:25 +0800

    modify

--- crawler/pywebsite_crawler.py
+++ crawler/pywebsite_crawler.py
@@ -1,11 +1,77 @@
+import requests
+from lxml import etree
+
+
 def judge_py_website(url):
     """
     判断url是一个与python非常相关的网站
     :param url:
     :return:
     """
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, compress',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+    }
 
+    res = requests.get(url, headers=headers)
+    res.encoding = 'utf-8'
+
+    if judge_py_site_by_keywords_description(res.text):
+        return True
+
+    if judge_py_site_by_menu(res.text):
+        return True
+
+    return False
+
+
+def judge_py_site_by_keywords_description(html):
+    """
+    用keywords description, 判断是否是python主题网站
+    :param html:
+    :return:
+    """
+    tree = etree.HTML(html)
+    if _judge_py_site_by_keywords_description_ex(tree, 'keywords'):
+        return True
+
+    if _judge_py_site_by_keywords_description_ex(tree, 'description'):
+        return True
+
+    return False
+
+
+def _judge_py_site_by_keywords_description_ex(tree, tag_name):
+    node_lst = tree.xpath("//meta[@name='{tag_name}']".format(tag_name=tag_name))
+    if node_lst:
+        node = node_lst[0]
+        content = node.attrib['content'].lower()
+        if content.find('python') != -1 and content.find('教程') != -1:
+            return True
+
+
+def judge_py_site_by_menu(html):
+    """
+    用菜单判断是否是python主题网站
+    :param html:
+    :return:
+    """
+    tree = etree.HTML(html)
+    a_lst = tree.xpath('//li/a')
+    for node in a_lst:
+        text = node.text
+        if len(text) <= 10 and text.lower().find('python') != -1:
+            return True
+
+    return False
 
 if __name__ == '__main__':
     url = 'http://www.kidscode.cn/python'
+    print(judge_py_website(url))
+
+    url = 'https://www.itcodemonkey.com/'
     print(judge_py_website(url))
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import requests
-import time
-from urllib.parse import quote
+from lxml import etree
 
 headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -8,29 +7,14 @@ headers = {
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'referer': quote('http://www.baidu.com/s?wd=python&pn=10'),
-        'Host': 'www.baidu.com',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
     }
 
-url = 'https://www.baidu.com/s'
+url = 'https://www.w3cschool.cn/python3'
+res = requests.get(url, headers=headers)
+res.encoding = 'utf-8'
 
-params = {
-            'wd': 'python 教程',
-            'pn': 0
-        }
-
-session = requests.session()
-
-def get_proxy():
-    return requests.get("http://127.0.0.1:5010/get/").text
-
-
-def get_baidu_data():
-    print(params)
-    res = session.get(url, params=params, headers=headers, allow_redirects=False)
-    res.encoding = 'utf-8'
-    print(res.status_code)
-
-get_baidu_data()
+tree = etree.HTML(res.text)
+keywords = tree.xpath('//meta[@name="keywords"]')
+print(keywords)
 
