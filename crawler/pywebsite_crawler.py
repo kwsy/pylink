98089d38703e9fdf7eb19e891a668e0333990ef4
LGWLK <weirenwuyi@126.com>
2020-03-15 23:33:23 +0800

    modified

--- crawler/pywebsite_crawler.py
+++ crawler/pywebsite_crawler.py
@@ -1,11 +1,80 @@
+from lxml import etree
+import requests
+
+
+
+
 def judge_py_website(url):
     """
     判断url是一个与python非常相关的网站
     :param url:
     :return:
     """
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, compress',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+    }
+
+    res = requests.get(url,headers=headers)
+    res.encoding = 'utf-8'
+    tree = etree.HTML(res.text)
+
+    if judge_by_keywords_description(tree):
+        return True
+    if judge_by_menu(tree):
+        return True
+
+    return False
+
+
+def judge_by_keywords_description(tree):
+    if _judge_by_keywords_description(tree,'keywords'):
+        return True
+    if _judge_by_keywords_description(tree,'description'):
+        return True
+
+    return False
+
+
+def _judge_by_keywords_description(tree,tag):
+    """
+    利用Keywords，description判断网站相关性
+    :param tree:
+    :param tag:
+    :return:
+    """
+    node = tree.xpath('//meta[@name="{tag}"]'.format(tag=tag))[0]
+    content = node.attrib['content'].lower()
+
+    if content.find('python') != -1 and content.find('教程') != -1:
+        return True
+
+    return False
+
+
+
+def judge_by_menu(tree):
+    """
+    利用网站菜单拦判断网站相关性
+    :param html:
+    :return:
+    """
+    menu_node = tree.xpath('//ul[@id="main-menu"]/li/a')
+    for menu_content in menu_node:
+        content = menu_content.text.lower()
+        if content.find('python') != -1 and content.find('教程') != -1:
+            return True
+    return False
+
 
 
 if __name__ == '__main__':
     url = 'http://www.kidscode.cn/python'
+    print(judge_py_website(url))
+
+    url = 'https://www.itcodemonkey.com/'
     print(judge_py_website(url))
--- crawler/zhihu_crawler.py
+++ crawler/zhihu_crawler.py
@@ -13,7 +13,7 @@ headers = {
 
 def get_zhuanlan_info(url):
     """
-    获取一个专栏的关键信息, 比如专栏的名称, 关注人数
+    获取专栏主页网站
     :param url:
     :return:
     """
@@ -27,37 +27,43 @@ def get_zhuanlan_info(url):
 
 
 def get_zhuanlan_info2(url):
+    """
+    获取关键信息，如专栏名，发表文章，共发布文章，关注人数等
+    :param url:
+    :return:
+    """
     session = requests.session()
     res = session.get(url, headers=headers)
     etree_html= etree.HTML(res.text)
 
     columns_name_node = etree_html.xpath('//div[@class="ContentItem-head"]')
+    lst = []
     for i in columns_name_node:
         name = i.xpath('.//h2/a/div/div')[0]
-        print(name.text)
-        # 问题1，为什么print会有2个值，return只有1个值
 
         specific_info = i.xpath('.//div[@class="ContentItem-status"]')[0]
         specific_info_text = specific_info.xpath('string(.)')
         specific_info_count = specific_info_text.split(' ')
 
-
         info = {
+            'column name': name.text,
             'publish': int(specific_info_count[1]),
             'total article': int(specific_info_count[3]),
-            'followers': int(''.join(re.findall('\d',specific_info_count[4])))
+            'followers': int(''.join(re.findall('\d+',specific_info_count[4])))
             # findall 提取字符串里的数字部分，join串联各个数字成为一个整体的数字
         }
 
-        print(info)
-        # 问题2：同问题1，为什么print会有2个值，return只有1个值
+        lst.append(info)
+
+    return lst
+
 
 
 
 if __name__ == '__main__':
     url = 'https://zhuanlan.zhihu.com/p/109450078'
     print(get_zhuanlan_info(url))
-    # 问题3：如何去掉最后打印处来的None
+
 
 
 
