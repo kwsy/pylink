8907ed4e5ebbe2043de5815e3bc7fd96dbde2c4a
huangjiafeng <498339146@qq.com>
2020-04-11 22:16:58 +0800

    modify0411-22:16

--- conf/mysql_conf.py
+++ conf/mysql_conf.py
@@ -1,31 +1,6 @@
-import pymysql
-
-
-class Connect_Mysql:
-    def __init__(self):
-        self.host = '101.201.225.172'
-        self.user = 'hjfdata'
-        self.pwd = '123456!'
-        self.port = '3309'
-        self.db = 'hjf_pylink'
-
-    def connectDatabase(self):
-        try:
-            self.conn = pymysql.connect(self.host, self.user,
-                           self.pwd, self.db, charset='utf8')
-        except:
-            print("connectDatabase failed")
-            return False
-        self.cur = self.conn.cursor()
-        return True
-
-def test():
-    sql = """select * from csdn_hjf;"""
-
-    cursor = Connect_Mysql.mysqlDB().cursor()
-    cursor.execute(sql)
-    cursor.close()
-
-
-if __name__ == '__main__':
-    test()
+hjf_mysql_setting = {'host': '101.201.225.172',
+                     'user': 'hjfdata',
+                     'pwd': '123456!',
+                     'port': '3309',
+                     'db': 'hjf_pylink',
+                     'charset': 'utf8'}
