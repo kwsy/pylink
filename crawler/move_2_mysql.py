39b9fe218c98acc0231c50d17999e0a287f82cd7
huangjiafeng <498339146@qq.com>
2020-04-12 18:02:59 +0800

    modify

--- crawler/move_2_mysql.py
+++ crawler/move_2_mysql.py
@@ -10,41 +10,49 @@ import re
 def move_py_web_site_to_mysql():
     datas = mongo_find_collect_all(MongoCollection.pywebsite_mongo)
     for data in datas:
-        netloc = get_netloc(data['href'])
-        sort = get_alexa_sort(netloc)
-        time.sleep(3)
-        data['insert_time'] = data['insert_time'].strftime("%Y-%m-%d")
-        info = {'href': data['href'], 'web_rank': sort, 'score': data['score'], 'insert_time': data['insert_time']}
-        update_object(PyWebsite, info)
+        print(data)
+        try:
+            netloc = get_netloc(data['href'])
+            sort = get_alexa_sort(netloc)
+            time.sleep(3)
+            data['insert_time'] = data['insert_time'].strftime("%Y-%m-%d")
+            info = {'href': data['href'], 'web_rank': sort, 'score': data['score'], 'insert_time': data['insert_time']}
+            update_object(PyWebsite, info)
+        except Exception as e:
+            print(e)
 
 
 def move_csdn_to_mysql():
     datas = mongo_find_collect_all(MongoCollection.csdn_mongo)
     for data in datas:
-        time.sleep(3)
-        data['insert_time'] = data['insert_time'].strftime("%Y-%m-%d")
-        print(data)
-        info = data
-        del info['_id']
-        update_object(Csdn_hjf, info)
+        try:
+            data['insert_time'] = data['insert_time'].strftime("%Y-%m-%d")
+            print(data)
+            info = data
+            del info['_id']
+            update_object(Csdn_hjf, info)
+        except Exception as e:
+            print(e)
+
 
 def move_zhihu_to_mysql():
     datas = mongo_find_collect_all(MongoCollection.zhihu_mongo)
     for data in datas:
-        time.sleep(3)
-        data['insert_time'] = data['insert_time'].strftime("%Y-%m-%d")
-        print(data)
-        for item in data['info']:
-            item['insert_time'] = data['insert_time']
-            item['href'] = data['href']
-            item['follower'] = int(''.join(re.findall('\d+', item['follower'])))
-            item['article_num'] = int(''.join(re.findall('\d+', item['article_num'])))
-            print(item)
-            update_object_zhihu(Zhihu_hjf, item)
+        try:
+            data['insert_time'] = data['insert_time'].strftime("%Y-%m-%d")
+            print(data)
+            for item in data['info']:
+                item['insert_time'] = data['insert_time']
+                item['href'] = data['href']
+                item['follower'] = int(''.join(re.findall('\d+', item['follower'])))
+                item['article_num'] = int(''.join(re.findall('\d+', item['article_num'])))
+                print(item)
+                update_object_zhihu(Zhihu_hjf, item)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
-
-    #move_py_web_site_to_mysql()
-    #move_csdn_to_mysql()
+    move_csdn_to_mysql()
     move_zhihu_to_mysql()
+    move_py_web_site_to_mysql()
