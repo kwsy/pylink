b643b3f15b26b3d947614145dc4eaa930055ddfe
kwsy <xigongda200608@163.com>
2020-06-14 19:33:13 +0800

    实现分页

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from db.mysql_client import PyWebsite, query_data
+from flask import Flask, render_template, request
+from db.mysql_client import PyWebsite, query_data, query_data_by_page
 
 
 app = Flask(__name__)
@@ -12,8 +12,13 @@ def index():
 
 @app.route('/py-site', methods=['GET'])
 def py_site():
-    datas = query_data(PyWebsite)
-    return render_template('py_site.html', py_data=datas)
+    page = int(request.args.get('page', 1))
+    count = int(request.args.get('count', 10))
+    datas, page_count = query_data_by_page(PyWebsite, page, count)
+    return render_template('py_site.html',
+                           py_data=datas,
+                           page_count=page_count,
+                           current_page=page)
 
 
 if __name__ == '__main__':
--- db/mysql_client.py
+++ db/mysql_client.py
@@ -58,6 +58,13 @@ def query(model):
 
     session.close()
 
+def query_data_by_page(model, page=1, count=10):
+    session = DBSession()
+    datas = session.query(model).order_by(model.score)
+    page_data = datas.limit(count).offset((page-1)*count)
+    page_count = datas.count()//count + 1
+    return page_data, page_count
+
 def create_table(table):
     BaseModel.metadata.create_all(engine, tables=[table.__table__])
 
